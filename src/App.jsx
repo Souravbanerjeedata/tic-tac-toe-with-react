d0e40c624f42b102ed586772d817bfecec71cf29
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2025-04-03 13:47:29 +0530

    Concept Repetition: Working with State

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Player from "./components/Player.jsx";
 
 function App() {
   return (
-    <div id="game-container">
-      <ol id="players">
-        <Player name="Player 1" symbol="X" />
-        <Player name="Player 2" symbol="O" />
-      </ol>
-      GAME BOARD
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   );
-  LOG;
 }
 
 export default App;
